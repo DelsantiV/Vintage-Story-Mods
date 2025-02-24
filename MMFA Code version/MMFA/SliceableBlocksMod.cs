d2ad8f765db6ed60fb3246dc2bfef5b2d0260432
DelsantiV <vincent.delsanti@gmail.com>
2025-02-24 16:21:33 +0100

    Initial commit

---
+++ MMFA Code version/MMFA/SliceableBlocksMod.cs
@@ -0,0 +1,21 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace MMFA
+{
+    public class SliceableBlocksMod : ModSystem
+    {
+        // Called on server and client
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("My Modest food adjustments was loaded !");
+
+            base.Start(api);
+            api.RegisterBlockBehaviorClass("Sliceable", typeof(BlockBehaviorSliceable));
+            api.RegisterBlockClass("SliceableBlock", typeof(SliceableBlock));
+            api.Logger.Notification("My Modest food adjustments classes successfully registered !");
+        }
+    }
+}
